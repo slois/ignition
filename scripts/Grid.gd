91eb0b1def9091f679dd05ade4a44cf969844fe4
slois <sergio.lois@gmail.com>
2019-10-08 21:14:34 +0200

    Spawn blocks and fill grid

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -3,34 +3,21 @@ extends TileMap
 var tile_size = get_cell_size()
 var half_tile_size = tile_size / 2;
 
-var grid_size = Vector2(16, 16)
+# Grid position: (x, y) x=columns; y=rows
+
+var grid_size = Vector2(9, 12)
 var grid = []
+var rnd = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	initialize_grid()
-	
-	# Examples
-	
-	var Block = get_node("Block")
-	var Block2 = get_node("Block2")
-	var Block3 = get_node("Block3")
-	var Block4 = get_node("Block4")
-	
-	var start_pos = update_child_pos(Block)
-	Block.position = start_pos
-	
-	Block2.position = grid2position(Vector2(0,15))
-	grid[0][15] = Block2
-	
-	Block3.position = grid2position(Vector2(1,0))
-	grid[1][0] = Block3
-	
-	Block4.position = grid2position(Vector2(0,4))
-	grid[0][4] = Block4
+	rnd.randomize()
 	
 func initialize_grid():
+	# Columns
 	for i in range(grid_size.x):
 		grid.append([])
+		# Rows
 		for j in range(grid_size.y):
 			grid[i].append(null)
 
@@ -57,6 +44,43 @@ func grid2position(position: Vector2):
 func is_empty(pos: Vector2):
 	return grid[pos.x][pos.y] == null
 
+func spawn_block(cell_pos: Vector2, fall: bool):
+	if fall:
+		cell_pos.y = -1
+	
+	var my_position = grid2position(cell_pos)
+
+	$Spawner.move_to(my_position)
+	var block = $Spawner.spawn_block()
+	add_child(block)
+	
+	block.position = my_position
+	grid[cell_pos.x][cell_pos.y] = block
+	print_grid()
+
+func get_available_cells():
+	var available_cells = []
+	for col in range(0, grid_size.x):
+		for row in range(0, grid_size.y-1):
+			var cell_pos = Vector2(grid_size.x - col - 1, grid_size.y - row - 1)
+			if is_empty(cell_pos):
+				available_cells.append(cell_pos)
+				break
+	return available_cells
+
 func _on_Timer_timeout() -> void:
-	pass
+	var free_cells = get_available_cells()
+	if len(free_cells) > 0:
+		var free_cell_pos = free_cells[rnd.randi_range(0, len(free_cells)-1)]
+		spawn_block(free_cell_pos, true)
+		
+func print_grid():
+	for row in range(0, grid_size.y-1):
+		for col in range(0, grid_size.x):
+			print(row, col, grid[col][row])
+			
+
+	
+	
+	
 	
